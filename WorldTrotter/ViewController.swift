8d21db3c562e81197edcf04ebc675a71e545378e
Tyler Percy <tpercy@ironman.highpoint.edu>
2019-01-30 13:18:35 -0500

    Initial Commit

---
+++ WorldTrotter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Tyler Percy on 1/30/19.
+//  Copyright © 2019 Tyler Percy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
